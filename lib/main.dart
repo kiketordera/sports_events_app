a6c0009961ac1a1d86d6a8c3a6957bb99b1011a9
Kike Tordera <dojranovski@gmail.com>
2024-10-10 01:14:23 -0400

    feat(ui): add logo, configure fonts and colors, and implement AppBar with Yesterday, Today, Tomorrow tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:sports_events_app/theme/colors.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SportsEventsApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SportsEventsApp extends StatelessWidget {
+  const SportsEventsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Sports Events',
+      theme: ThemeData(
+        fontFamily: 'Inter',
+      ),
+      home: const EventsHomePage(),
+    );
+  }
+}
+
+class EventsHomePage extends StatelessWidget {
+  const EventsHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColorDark,
+          centerTitle: true,
+          title: Image.asset(
+            'assets/img/logo.png',
+            height: 25,
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(44),
+            child: Container(
+              color: AppColors.backgroundColorGray,
+              child: const TabBar(
+                indicatorColor: AppColors.selectedColor,
+                labelColor: AppColors.selectedColor,
+                unselectedLabelColor: AppColors.unselectedColor,
+                tabs: [
+                  Tab(text: 'YESTERDAY'),
+                  Tab(text: 'TODAY'),
+                  Tab(text: 'TOMORROW'),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Text('Here events for today'),
+            Text('Here events for yesterday'),
+            Text('Here events for tomorrow'),
+          ],
         ),
       ),
     );
---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color backgroundColorDark = Color(0xFF171E31);
+  static const Color backgroundColorGray = Color(0xFFF3F3F3);
+  static const Color hoverColor = Color(0xFFE7F4F8);
+  static const Color selectedColor = Color(0xFF26AEFB);
+  static const Color unselectedColor = Color(0xFF565F69);
+}
